5d074915a0bcf2207c9d0c3317d11273b0efde24
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-04-13 18:00:22 +0530

    Project Setup Done

---
+++ Swift Challenge/Swift Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift Challenge
+//
+//  Created by Jay Shah on 13/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Challenge/Swift Challenge/Swift_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ChallengeApp.swift
+//  Swift Challenge
+//
+//  Created by Jay Shah on 13/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
