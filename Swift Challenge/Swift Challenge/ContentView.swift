b8ad4d6151ab3a6944908f962d31793c8c82e861
jay-a-shah <jay.a.shah@simformsolutions.com>
2022-04-21 20:29:43 +0530

    Game Added

--- Swift Challenge/Swift Challenge/ContentView.swift
+++ Swift Challenge/Swift Challenge/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerOnePoints = 0
+    @State var playerTwoPoints = 10
+    @State var playerOneImage = "Stone"
+    @State var playerTwoImage = "Paper"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+    
+        VStack{
+            Text("Points: \(playerOnePoints)")
+                .font(.system(size: 25))
+                .bold()
+
+            Image(playerOneImage)
+                .resizable()
+                .frame(width: 400.0, height: 400.0)
+                .onTapGesture {
+                    let myTap = Int.random(in: 1...3)
+                    playerOneImage = imageGenerator(myTap)
+                }
+        
+            Image(playerTwoImage)
+                .resizable()
+                .frame(width: 400.0, height: 400.0)
+                .onTapGesture {
+                    let myTap = Int.random(in: 1...3)
+                    playerTwoImage = imageGenerator(myTap)
+                }
+            
+            Text("Points: \(playerTwoPoints)")
+                .font(.system(size: 25))
+                .bold()
+        }
+    }
+    
+    func imageGenerator(_ num: Int)-> String{
+        if num == 1{
+            return "Stone"
+        }else if num == 2 {
+            return "Paper"
+        }else if num == 3 {
+            return "Scissors"
+        }
+        return "Stone"
     }
 }
 
